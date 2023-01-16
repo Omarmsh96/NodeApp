ad5fcadc6934a2f6ba3279526c2d24d2a8226fd7
Steffi <steffi@MacBook-Air-von-Neu.local>
2023-01-16 23:06:33 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ const passwordDB = 'example' ;
 const portDB =27017; 
 const hostdb = '172.29.0.2'; 
 
-const URI = 'mongodb://userDB:passwordDB@hostdb:portDB';
+const URI = 'mongodb://userDB:passwordDB@172.29.0.2:27017';
 mongoose.connect(URI).then( () => console.log('connect to db .. ') )
 .catch((err) => console.log('failed to connect to db:', err));
 
 app.get ('/', (req, res) => {
-    res.send(" <h1>A heading here hi  dev </h1>")
+    res.send(" <h1>A heading here hi  dev used docker hub </h1>")
 });
 
 app.listen(PORT, () => console.log('app is running on port: ${PORT}'));
